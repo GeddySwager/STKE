feac9ac87b4bd0ba0b8f04eda308d9a988becb8a
GeddySwager <geddy@waveville.net>
2023-01-03 23:43:10 -0600

    Have been stuck for a few days on the best way to handle user input and trying to implement tab autocomplete.

--- STKE/STKE_user/STKE_user.c
+++ STKE/STKE_user/STKE_user.c
@@ -6,17 +6,74 @@ This program is used to interact with its corresponding kernel driver.
 #include <psapi.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <strsafe.h>
 #include <Shlwapi.h>
 
 // To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
 // and compile with -DPSAPI_VERSION=1
 
 #define ARRAY_SIZE 1024
+#define IOCTL_STKE_LOAD_DRIVER CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
-int Error(const char* msg)
+int Error(LPCTSTR lpszFunction)
 {
-    printf("%s: error=%d\n", msg, GetLastError());
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+
+    // Display the error message and clean up
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen(lpszFunction) + 40) * sizeof(TCHAR));
+    if (lpDisplayBuf != NULL)
+    {
+        StringCchPrintf((LPTSTR)lpDisplayBuf,
+            LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+            TEXT("%ws failed with error %d: %ws"),
+            lpszFunction, dw, (LPTSTR)lpMsgBuf);
+        printf("%ws", (LPTSTR)lpDisplayBuf);
+    }
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
     return 1;
+}
+
+
+BOOL GetInput(PWCHAR buf, DWORD charsToRead)
+{
+    BOOL status = TRUE;
+    DWORD charCount = 0, ch, dwConsoleMode;
+    HANDLE console = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(console, &dwConsoleMode);
+    printf("%x\n", dwConsoleMode);
+    printf("%x\n", (dwConsoleMode & ~(ENABLE_LINE_INPUT)));
+    //SetConsoleMode(console, (dwConsoleMode & ~(ENABLE_LINE_INPUT)));
+    //Error(L"SetConsoleMode");
+    printf("%x\n", dwConsoleMode);
+    do {
+        status = ReadConsoleW(console, &buf[charCount], 1, &ch, NULL);
+        charCount++;
+    } while (((charCount + 1) < charsToRead) && (buf[charCount] != '\r'));
+    /*if (charCount > charsToRead)
+    {
+        printf("You entered too many characters.\n");
+        CloseHandle(console);
+        return 0;
+    }*/
+
+    SetConsoleMode(console, dwConsoleMode);
+
+    return status;
 }
 
 // code from: https://learn.microsoft.com/en-us/windows/win32/psapi/enumerating-all-device-drivers-in-the-system
@@ -55,27 +112,28 @@ int endsWithSys(WCHAR* string)
     string = StrRChrW(string, NULL, L'.');
     if (string != NULL)
     {
-        return(wcscmp(string, L".sys"));
+        return (wcscmp(string, L".sys"));
     }
-    return(-1);
+    return 1;
 }
 
 int loadDriver()
 {
-    printf("Type out the full path to the driver, including driver name (example \"C:\\Users\\vagrant\\desktop\\driver.sys\")\n");
-    WCHAR regPath[MAX_PATH] = { 0 };
+    printf("Type out the full path to the driver, including driver name (example \"C:\\Users\\vagrant\\desktop\\driver.sys\")\n\n");
+    printf("Windows accepts a maximum path of 260 characters.\n\n");
+    WCHAR regPath[MAX_PATH + 2] = { 0 };
+    
 
     while (TRUE)
     {
-        int status = wscanf_s(L"%s", regPath, (MAX_PATH - 1));
-        if (!status || status == EOF)
+        if (!GetInput(regPath, _countof(regPath)))
         {
-            printf("Windows accepts a maximum path of 260 characters.\n");
+            printf("Somehow broke ReadConsole function.\n");
         }
         else if (!wcscmp(regPath, L"q"))
         {
             printf("Exiting.\n");
-            return -1;
+            return 1;
         }
         else if (endsWithSys(regPath))
         {
@@ -86,7 +144,6 @@ int loadDriver()
             DWORD dwAttrib = GetFileAttributes(regPath);
             if (dwAttrib != INVALID_FILE_ATTRIBUTES && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY))
             {
-                if (regPath)
                 break;
             }
             else
@@ -95,44 +152,111 @@ int loadDriver()
             }
         }
     }
-    printf("Driver found! Returning to start menu as feature is not finished.\n");
+    printf("Driver found! Please provide a name for your driver (limit 8 characters).\n");
+
+    WCHAR driverName[10] = { 0 };
+
+    while (TRUE)
+    {
+        if (!GetInput(driverName, _countof(driverName)))
+        {
+            printf("Somehow broke ReadConsole function.\n");
+        }
+        else if (!wcscmp(driverName, L"q"))
+        {
+            printf("Exiting.\n");
+            return 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    HKEY key;
+    DWORD keyOptions = REG_OPTION_VOLATILE;
+    WCHAR subKey[] = L"SYSTEM\\CurrentControlSet\\Services\\";
+    WCHAR keyValue1[] = L"ImagePath";
+    WCHAR keyValue2[] = L"Type";
+
+    WCHAR keyData[MAX_PATH + 4];
+    ZeroMemory(keyData, sizeof(keyData));
+    if (FAILED(StringCchCopyW(keyData, (MAX_PATH + 4), L"\\??\\")))
+    {
+        return Error(L"String copy failed.\n");
+    }
+    if (FAILED(StringCchCatW(keyData, (MAX_PATH + 4), regPath)))
+    {
+        return Error(L"String concatenation failed.\n");
+    }
+    printf("Test: %ws\n", keyData);
+
+
+
+    LSTATUS status = RegCreateKeyExW(HKEY_LOCAL_MACHINE, subKey, 0, NULL, keyOptions, KEY_ALL_ACCESS | KEY_WOW64_64KEY, NULL, &key, NULL);
+
+    if (status != ERROR_SUCCESS)
+    {
+        return Error(L"Failed to create registry key.\n");
+    }
+
+    //status = RegSetKeyValueW(key, NULL, )
+
+    HANDLE hDevice = CreateFile(L"\\\\.\\STKE", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        return Error(L"Failed to open device.\n");
+    }
+
+
+
     return 0;
 }
 
 
 int main(void)
 {
-    int option;
+    LONG option;
+    WCHAR selection[3];
     while (TRUE)
     {
         printf("\nSTKE available functions:\n");
         printf("1. List all installed or running drivers\n");
         printf("2. Load driver\n");
-        printf("0. Stop program\n");
-        wscanf_s(L"%d", &option);
-
-        if (option > 2)
+        printf("0. Stop program\n\n");
+        if(!(GetInput(selection, 3)))
         {
-            printf("\nPlease select from the available options.\n");
+            printf("Somehow broke ReadConsole function.\n");
+        }
+        else if (!(isdigit(selection[0])))
+        {
+            printf("You did not enter a number.\n");
         }
         else
         {
-            switch (option)
+            option = wcstol(selection, NULL, 0);
+            if (option > 2)
             {
-            case 0:
-                return 0;
-                break;
-            
-            case 1:
-                ListDrivers();
-                break;
-
-            case 2:
-                loadDriver();
-                break;
+                printf("\nPlease select from the available options.\n");
             }
+            else
+            {
+                switch (option)
+                {
+                case 0:
+                    return 0;
+                    break;
 
-        }
+                case 1:
+                    ListDrivers();
+                    break;
+
+                case 2:
+                    loadDriver();
+                    break;
+                }
+            }
+        } 
     }
 
     return 0;
